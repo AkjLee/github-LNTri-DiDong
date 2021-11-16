922bf5cecd6185ef44a276893c1a47b9df732153
AkjLee <trilee2912@gmail.com>
2021-11-16 21:17:34 +0700

    update

---
+++ lib/util/api_key.dart
@@ -0,0 +1,1 @@
+const String apiKey = '83ebc18109eb46d4b8fd12aa7c18d510';
---
+++ lib/util/topics.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_news/models/topic.dart';
+
+List<Topic> topics = [
+  Topic('Business', 'http://hanoimoi.com.vn/Uploads/images/phananh/2020/01/08/anhdulich1.jpg'),
+  Topic('Entertainment',
+      'http://hanoimoi.com.vn/Uploads/images/phananh/2020/01/08/anhdulich1.jpg'),
+  Topic('General',
+      'https://www.rainstormfilm.com/wp-content/uploads/thiet-ke-quang-cao-3d-rao-vat.jpg'),
+];
